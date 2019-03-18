2f06afde3b1b1d6ede9b967fc98119745704b81b
bigfoolliu <liu15671677014@icloud.com>
2019-03-18 18:12:47 +0800

    fix a little bug

--- basic/knn.py
+++ basic/knn.py
@@ -43,22 +43,10 @@ def read_file(file_path):
     """
     读取csv文件
     movie,actions,kisses,type
+    :return type: DataFrame
     """
     movie_data = pd.read_csv(file_path)
-    # 遍历movie_data
-    movie_names = []
-    actions = []
-    kisses = []
-    types = []
-    for index in range(len(movie_data)):
-        movie_names.append(movie_data.loc[index][0])
-        actions.append(movie_data.loc[index][1])
-        kisses.append(movie_data.loc[index][2])
-        types.append(movie_data.loc[index][3])
-
-    movie_names, actions, kisses, types = np.array(movie_names), np.array(actions), np.array(kisses), np.array(types)
-
-    return movie_names, actions, kisses, types
+    return movie_data
 
 
 def draw_scatter(x, y, color="red"):
@@ -71,7 +59,7 @@ def draw_scatter(x, y, color="red"):
 movie = read_file(FILE_PATH)
 # 以actions的数量作为横坐标，kisses作为纵坐标
 fig = plt.figure()
-plt.scatter(movie[1], movie[2])
+plt.scatter(movie["actions"], movie["kisses"])
 plt.xlabel("actions")
 plt.ylabel("kisses")
 plt.show()
