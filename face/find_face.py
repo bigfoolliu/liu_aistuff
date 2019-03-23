38dcf9d01a947fe969fcc1281c04bc79b8169c86
bigfoolliu <liu15671677014@icloud.com>
2019-03-24 00:26:31 +0800

    find_face add more functions

--- face/find_face.py
+++ face/find_face.py
@@ -44,6 +44,16 @@ def make_up():
         d.polygon(face_landmarks["top_lip"], fill=(150, 0, 0, 128))
         d.polygon(face_landmarks["bottom_lip"], fill=(150, 0, 0, 128))
         d.line(face_landmarks["top_lip"], fill=(150, 0, 0, 64), width=8)
+        d.line(face_landmarks["bottom_lip"], fill=(150, 0, 0, 64), width=8)
+
+        # 眼睛
+        d.polygon(face_landmarks["left_eye"], fill=(255, 255, 255, 30))
+        d.polygon(face_landmarks["right_eye"], fill=(255, 255, 255, 30))
+
+        # 眼线
+        # d.line(face_landmarks["left_eye"] + [face_landmarks["left_eye"][0]], fill=(0, 0, 0, 110), width=6)
+        # d.line(face_landmarks["right_eye"] + [face_landmarks["left_eye"][0]], fill=(0, 0, 0, 110), width=6)
+
         pil_image.show()
 
 
