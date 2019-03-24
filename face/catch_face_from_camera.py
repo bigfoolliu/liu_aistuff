e92a0a6cd5846fddda6a08976ca15568dcb2be0e
bigfoolliu <liu15671677014@icloud.com>
2019-03-24 16:53:10 +0800

    fix catch_face_from_camera.py bug

--- face/catch_face_from_camera.py
+++ face/catch_face_from_camera.py
@@ -1,27 +1,29 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 #!coding:utf-8
 
 
 """
 实时从摄像头中捕捉人脸
 """
-import face_reconition as face_r
+import face_recognition as face_r
 import cv2
 
 
 SAMPLE_IMAGE = "./known_images/tongliya.jpeg"
+print("[INFO]Use file {}".format(SAMPLE_IMAGE))
 
-# 使用摄像头
+# 使用摄像头(注意虚拟机的话需要打开摄像头设备)
 video_capture = cv2.VideoCapture(0)
+print("[INFO]video_capture: {}".format(video_capture))
 
 # 加载一张模板图片并辨别(可加载多张)
 tongliya_image = face_r.load_image_file(SAMPLE_IMAGE)
 tongliya_face_encoding = face_r.face_encodings(tongliya_image)[0]
-print("[INFO]tonyliya_image: {},tongliya_face_enciding: {}".format(tongliya_image, tongliya_face_encoding))
+print("[INFO]tonyliya_image: {},tongliya_face_encoding: {}".format(tongliya_image, tongliya_face_encoding))
 
 # 创建已知图片面部的数组以及名字
-known_face_encodings = [tongliya_face_encoding, ]
-known_face_names = ["tong li ya", ]
+known_face_encodings = [tongliya_face_encoding]
+known_face_names = ["tongliya"]
 
 
 # 初始化相关变量
@@ -36,6 +38,7 @@ print("[INFO]Begin to capture video")
 while True:
     # 从摄像头抓取单帧并处理
     ret, frame = video_capture.read()
+    print("[INFO]ret: {}, frame: {}".format(ret, frame))
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)  # 图片缩小
     rgb_small_frame = small_frame[:, :, ::-1]  # 将图片从BGR颜色(opencv使用)转换为rgb颜色(face_recognition使用)
 
@@ -70,14 +73,14 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # 放置一个面部的名字
-        cv2.rectangle(fram, (left, bottom-35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom-35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left+6, bottom-6), font, 1.0, (255, 255, 255), 1)
     # 展示图片结果
     cv2.imshow("Video", frame)
 
     # 按q键来退出
-    if cv2.waitKey(1) & oxff == ord("q"):
+    if cv2.waitKey(1) & 0xff == ord("q"):
         break
 
 # 释放摄像头
