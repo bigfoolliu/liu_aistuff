f675b8507dba2fd29afd0e26c765d116e03dce6c
bigfoolliu <liu15671677014@icloud.com>
2019-03-31 16:55:06 +0800

    add some codes

---
+++ 100_day/day1_preprocessing.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+数据预处理
+"""
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer  # 新
+# from sklearn.preprocessing import Imputer  # 旧
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+FILE_PATH = "datasets/Data.csv"
+
+
+# step1: 获取数据
+# iloc通过行号来索引行数据
+dataset = pd.read_csv(FILE_PATH)
+X = dataset.iloc[:, : -1].values  # 取所有的行,列取到倒数第二行(倒数第一行不取)
+Y = dataset.iloc[:, 3].values
+
+print("[INFO]The data we got is:")
+print("[INFO]X: {}\nY: {}\n".format(X, Y))
+
+
+# step2: 预处理数据
+# 处理丢失的数据
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")  # 新
+# imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)  # 旧
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+print("[INFO]After preprocessing:")
+print("[INFO]X: {}\n".format(X))
+
+
+# step3: 对明确的数据进行编码
+label_encoder_X = LabelEncoder()
+X[:, 0] = label_encoder_X.fit_transform(X[:, 0])
+
+# 创建一个dummy变量
+one_hot_encoder = OneHotEncoder(categorical_features=[0])
+X = one_hot_encoder.fit_transform(X).toarray()
+label_encoder_Y = LabelEncoder()
+Y = label_encoder_Y.fit_transform(Y)
+
+print("[INFO]After encoding:")
+print("[INFO]X: {}\nY: {}\n".format(X, Y))
+
+
+# step4: 将数据分割为训练数据和测试数据
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=0
+    )
+
+
+# step5: 特征提取
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+
+print("[INFO]After scaling the features:")
+print("[INFO]X_train: {}\nX_test: {}\n".format(X_train, X_test))
---
+++ 100_day/day2_linerRegression.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+简单的线性回归实例
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+# step1:获取数据
+dataset = pd.read_csv("datasets/studentscores.csv")
+X = dataset.iloc[:, :1].values
+Y = dataset.iloc[:, 1].values
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.25, random_state=0
+    )
+
+# step2:将线性回归模型应用到训练集
+regressor = LinearRegression()
+regressor = regressor.fit(X_train, Y_train)
+
+
+# step3:预测结果
+Y_pred = regressor.predict(X_test)
+
+
+# step4:可视化结果
+plt.scatter(X_train, Y_train, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="blue")
+
+plt.scatter(X_test, Y_test, color="green")
+plt.plot(X_test, regressor.predict(X_test), color="blue")
+
+# plt.legend()
+plt.show()
