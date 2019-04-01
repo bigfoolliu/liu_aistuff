a225570bc364c7222af6196fd8efe41d51345312
bigfoolliu <liu15671677014@icloud.com>
2019-04-01 23:56:13 +0800

    change

--- 100_day/day6_logisticRegression.py
+++ 100_day/day6_logisticRegression.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# --×--coding --*--
+# --*- coding:utf-8 -*-
 
 
 """
@@ -10,6 +10,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 
 # step1:数据预处理
@@ -25,3 +27,15 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
+
+# step2:构造logistic回归模型
+classifier = LogisticRegression()
+classifier.fit(X_train, Y_train)
+
+
+# step3:预测
+Y_pred = classifier.predict(X_test)
+
+
+# step4:评估预测结果
+cm = confusion_matrix(Y_test, Y_pred)
