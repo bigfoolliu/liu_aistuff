73c939cfc12c849bbab830e9305dc9296000afb0
bigfoolliu <liu15671677014@icloud.com>
2019-06-18 00:12:54 +0800

    add unittest

--- basic/sklearn_learning/1_linear_regression/test_linear_regression.py
+++ basic/sklearn_learning/1_linear_regression/test_linear_regression.py
@@ -8,10 +8,6 @@ import numpy as np
 import unittest
 from linear_model import LinearRegressionModel
 
-lr_test.run()
-print(lr_test.lr.coef_)
-print(lr_test.lr.intercept_)
-
 
 class TestLinearRegressionModel(unittest.TestCase):
 
@@ -19,9 +15,9 @@ class TestLinearRegressionModel(unittest.TestCase):
 
     def setUp(self):
         """每个测试用例执行前做的环境配置"""
-        x_train = np.array([1, 2, 3])
-        y_train = np.array([4, 5, 6])
-        lr_test = LinearRegressionModel(x_train, y_train)
+        self.x_train = np.array([1, 2, 3])
+        self.y_train = np.array([4, 5, 6])
+        self.lr_test = LinearRegressionModel(self.x_train, self.y_train)
     
     def tearDown(self):
         """每个测试用例执行结束之后做的环境清理"""
@@ -29,3 +25,13 @@ class TestLinearRegressionModel(unittest.TestCase):
     
     def test_linear_regression_model(self):
         """测试方法以test开头"""
+        self.lr_test.run()
+        print(self.lr_test.lr.coef_)
+        assert self.lr_test.lr.coef_ == 1
+        print(self.lr_test.lr.intercept_)
+        assert self.lr_test.lr.intercept_ == 3
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
