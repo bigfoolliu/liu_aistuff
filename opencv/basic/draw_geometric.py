6716304f2ceca91513f9e6bea8a91f7d79c3ed17
bigfoolliu <liu15671677014@icloud.com>
2019-06-30 19:21:52 +0800

    add geometrc

---
+++ opencv/basic/draw_geometric.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#author: bigfoolliu
+
+
+"""
+画几何图形
+"""
+
+
+import cv2
+import numpy as np
+
+
+img = np.zeros((512, 512, 3), np.uint8)
+
+# 画直线
+img = cv2.line(img, (0, 0), (500, 500), (255, 0, 0), 5)
+
+# 画矩形
+img = cv2.rectangle(img, (0, 0), (200, 100), (0, 255, 0), 3)
+
+# 画圆
+img = cv2.circle(img, (100, 100), 50, (0, 0, 255), -1)
+
+# 画椭圆
+# img = cv2.ellipse(img, (200, 200), (100, 50), 0, 0, 180, 255, -1)  # 半个椭圆
+img = cv2.ellipse(img, (200, 200), (100, 50), 0, 0, 360, 255, -1)
+
+cv2.imshow("geometric", img)
+
+while True:
+    if cv2.waitKey(1000) == 27:
+        break
