7d9455aaada61e2344bf93ca42e61ca84499d2e2
bigfoolliu <liu15671677014@icloud.com>
2019-07-03 01:32:23 +0800

    add put test

--- opencv/basic/draw_geometric.py
+++ opencv/basic/draw_geometric.py
@@ -27,6 +27,10 @@ img = cv2.circle(img, (100, 100), 50, (0, 0, 255), -1)
 # img = cv2.ellipse(img, (200, 200), (100, 50), 0, 0, 180, 255, -1)  # 半个椭圆
 img = cv2.ellipse(img, (200, 200), (100, 50), 0, 0, 360, 255, -1)
 
+# 添加文字
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, "open cv ", (10, 300), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
 cv2.imshow("geometric", img)
 
 while True:
