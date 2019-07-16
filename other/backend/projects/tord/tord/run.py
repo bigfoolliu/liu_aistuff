c8cd3f51b4244a9a9d761d10dee71c1d6ba9ee71
bigfoolliu <liu15671677014@icloud.com>
2019-07-17 02:43:08 +0800

    tornado add basehandler

--- other/backend/projects/tord/tord/run.py
+++ other/backend/projects/tord/tord/run.py
@@ -12,19 +12,38 @@ from tornado.options import define, options
 define("port", type=int, default=8000, help="server port")
 
 
-class IndexHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        raise NotImplementedError
+    
+    def post(self):
+        raise NotImplementedError
+    
+    def write_success(self, reason=None):
+        self.set_status(200, reason=reason)
+        self.finish()
+
+
+class IndexHandler(BaseHandler):
 
     def get(self):
         self.write("hello, tornado")
-        self.set_status(200)
-        self.finish()
+        self.write_success()
+
+
+class BooksHandler(BaseHandler):
+
+    def get(self, data):
+        self.write(data)
+        self.write_success(reason="return ok")
 
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-        [(r"/", IndexHandler),],
-        )
+        [(r"/", IndexHandler),
+        (r"/books/(\w+)", BooksHandler),],)
 
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
