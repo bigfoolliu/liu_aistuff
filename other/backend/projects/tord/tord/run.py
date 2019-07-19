8863c8b8830a80e752cd545c8be8836d67507c34
bigfoolliu <liu15671677014@icloud.com>
2019-07-19 23:46:44 +0800

    find a better way to stop tornado with ctrl c

--- other/backend/projects/tord/tord/run.py
+++ other/backend/projects/tord/tord/run.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 #author: bigfoolliu
 
+import logging
 import os
 
 import tornado.httpserver
@@ -10,18 +11,22 @@ import tornado.options
 import tornado.web
 from tornado.options import define, options
 
-from tord.handlers import upload, index
+from tord.handlers import index, upload
 
 define("port", type=int, default=8000, help="server port")
 
 
 def main():
     tornado.options.parse_command_line()
+
     app = tornado.web.Application(
         [(r"/", index.IndexHandler),
         (r"/books", upload.BooksHandler),
         ],)
 
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+    try:
+        app.listen(options.port)
+        logging.info("listening on port {}".format(options.port))
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop()
