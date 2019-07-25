61d1975f14f02518374c979876b0e57d8ee5b941
bigfoolliu <liu15671677014@icloud.com>
2019-07-26 02:50:45 +0800

    add tree

---
+++ algorithms/backend/binary_tree/tree.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#author: bigfoolliu
+
+
+"""
+this will show all kinds of type of tree the same time,
+and it will be a long time project
+"""
+class Tree(object):
+
+    tree_types = []
+
+    def __init__(self):
+        pass
+    
+    def types(self):
+        pass
+    
+    def display(self):
+        pass
