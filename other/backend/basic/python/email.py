9043d481475ce6bf913735be7bb4be885066f8bb
bigfoolliu <liu15671677014@icloud.com>
2019-08-06 00:10:19 +0800

    add

--- other/backend/basic/python/email.py
+++ other/backend/basic/python/email.py
@@ -15,3 +15,4 @@ port = 25  # smtp默认的端口号设置为25
 local_hostname = ""  #  如果 SMTP 在你的本机上，你只需要指定服务器地址为 localhost 即可
 
 smtp_obj = smtplib.SMTP(host=host, port=port, local_hostname=local_hostname)
+smtp_obj.sendmail(from_addr="", to_addrs="", msg="", mail_options=[])
