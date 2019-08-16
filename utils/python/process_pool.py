e6f767c70c0a26510d090a3ac9e0a7270b73eff9
tonyliu <liurui@jiangxing.ai>
2019-08-16 20:38:51 +0800

    add

--- utils/python/process_pool.py
+++ utils/python/process_pool.py
@@ -10,7 +10,16 @@ import os
 import random
 
 
-def task(msg):
+# def task(msg, name=None):
+#     t_start = time.time()
+#     print("begin task {}, id is {}".format(msg, os.getpid()))
+#     time.sleep(random.random() * 2)
+#     t_stop = time.time()
+#     print("name is {}".format(name))
+#     print("end task {}, spend time {}".format(msg, t_stop - t_start))
+
+
+def task(msg=None):
     t_start = time.time()
     print("begin task {}, id is {}".format(msg, os.getpid()))
     time.sleep(random.random() * 2)
@@ -21,7 +30,9 @@ def task(msg):
 if __name__ == "__main__":
     po = multiprocessing.Pool(3)
     for i in range(0, 10):
-        po.apply_async(task, (i,))  # apply_asyc使用非阻塞方式调用函数
+        # po.apply_async(task, (i,), {"name": i})  # apply_asyc使用非阻塞方式调用函数
+        # po.apply_async(task, (i,), {"name": i})  # apply_asyc使用非阻塞方式调用函数
+        po.apply_async(task, (), {"msg": i})  # apply_asyc使用非阻塞方式调用函数
     print("------start------")
     po.close()  # 关闭进程池，不再接受新的任务(po.terminate则是不管任务是否完成，立即终止)
     po.join()  # 主进程阻塞，等待子进程退出，必须在close和terminate之后使用
