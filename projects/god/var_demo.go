1c31a95f8a63bfb6e65178658cadd4dc5d785b31
bigfoolliu <liu15671677014@icloud.com>
2019-08-21 02:43:54 +0800

    add

--- projects/god/var_demo.go
+++ projects/god/var_demo.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "unsafe"
+)
 
 /*
 声明变量的示例
@@ -14,4 +17,25 @@ func main() {
 
 	var b, c int = 1, 2
 	fmt.Println(b, c)
+
+	const length int = 10
+	print(length)
+
+	a = "abc"
+    b = len(a)
+	// c = unsafe.Sizeof(a)
+	println(a, b, c)
+
+	var r bool
+	var c1, c2 bool = true, false
+	r = c1 && c2
+	if r {
+		print("c1&&c2:", r)
+	}
+
+	var r1 bool
+	r1 = c1 || c2
+	if r1 {
+		print("c1||c2", r1)
+	}
 }
