5269f5e6fe17887d1207bb9539b7da8f2a47445f
bigfoolliu <liu15671677014@icloud.com>
2019-08-26 02:21:49 +0800

    fabric add

---
+++ projects/tord/fabfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+"""
+fabric介绍：
+https://blog.csdn.net/freeking101/article/details/81103945
+"""
+
+
+def hello():
+    print("hello")
