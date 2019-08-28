298ae60b6a5009805631bcfed00023a05c9d1eeb
tonyliu <liurui@jiangxing.ai>
2019-08-28 14:31:51 +0800

    tord add more

--- projects/tord/tord/handlers/gocron.py
+++ projects/tord/tord/handlers/gocron.py
@@ -9,5 +9,19 @@ class TestHandler(base.BaseHandler):
 
     def post(self):
         data = self.request.body
+        print("body:", data, type(data))
+
+        parameters = self.parse_query_arguments()
+        print("parse_query_arguments:", parameters)
+
+        name = self.get_argument("name")
+        print("get_argument: ", name)
+
+        body_arguments = self.get_body_arguments
+        print("get_body_arguments: ", body_arguments, self.get_body_arguments("name"))
+
+        query_arguments = self.request.query_arguments
+        print("query_arguments: ", query_arguments, self.get_query_arguments("name"))
+
         self.write(data)
         self.finish()
