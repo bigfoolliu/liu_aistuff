bdf54301277d47ea90005c0daeca679584f30ac4
bigfoolliu <liu15671677014@icloud.com>
2019-09-01 21:07:04 +0800

    modules learning add

--- data_structure/link_list/single_link_list.py
+++ data_structure/link_list/single_link_list.py
@@ -19,17 +19,44 @@ class SingleLinkList(object):
     def __init__(self):
         self.__head = None
 
-    def add(self, item):
+    def insert_head(self, data):
         """在头部插入节点"""
         node = SingleNode(item)
-        node.next = self.__head
+        if self.__head:
+            node.next = self.__head
         self.__head = node
 
-    def append(self, item):
+    def insert_tail(self, data):
         """尾部插入节点"""
-        if self.__head:  # 非空
+        if not self.__head:
+            self.add(data)
+        else:
             cur = self.__head
-            while cur:
+            while cur:  # 遍历到最后一个节点
                 cur = cur.next
-            node = SingeNode(item)
-            cur.next = node
+            cur.next = SingleNode(data)
+    
+    def delete_head(self):
+        """删除头部节点"""
+        cur = self.__head
+        if self.__head:
+            self.__head = self.__head.next
+            cur.next = None
+        return cur
+    
+    def delete_tail(self):
+        """删除尾部节点"""
+        cur = self.__head:
+        if self.__head:
+            if not self.__head.next:
+                self.__head = None  # 只有一个头节点
+            else:
+                while cur.next.next:  # 找到倒数第二个节点
+                    cur = cur.next
+                cur.next, cur = None, cur.next
+        return cur
+    
+    def is_empty(self):
+        """判断单链表是否为空"""
+        return self.__head is None
+    
