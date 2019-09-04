02e801880d3f6b78546c5147fa66bacb4d4dd5f7
bigfoolliu <liu15671677014@icloud.com>
2019-09-05 01:52:05 +0800

    tk video add

--- projects/tk_video/interface.py
+++ projects/tk_video/interface.py
@@ -8,22 +8,39 @@
 import tkinter
 
 
-class Interface(tkinter.Frame):
+class PlayBottonInterface(object):
 
-    def __init__(self, master=None):
+    def __init__(self, master):
         """默认master设置为None,可以设置为其他顶层窗口的一个组件"""
-        tkinter.Frame.__init__(self, master)
-        self.pack()
+        self.master = master
         self.init_widgets()
     
     def init_widgets(self):
         """初始化该frame上的一些GUI组件"""
-        start_button = tkinter.Button(self, text="start", background="red")
-        start_button.pack()
-        pause_button = tkinter.Button(self, text="pause")
-        pause_button.pack()
-        stop_button = tkinter.Button(self, text="stop")
-        stop_button.pack()
+        frame = tkinter.Frame(self.master, height=400, width=300, background="yellow")
+        frame.pack(side=tkinter.BOTTOM, fill=tkinter.BOTH)  # frame放置在底部
 
-interface = Interface()
-interface.mainloop()
+        start_button = tkinter.Button(frame, text="start")
+        start_button.pack(side=tkinter.LEFT, expand=1, fill=tkinter.BOTH, padx=2)
+        pause_button = tkinter.Button(frame, text="pause")
+        pause_button.pack(side=tkinter.LEFT, expand=1, fill=tkinter.BOTH, padx=2)
+        stop_button = tkinter.Button(frame, text="stop")
+        stop_button.pack(side=tkinter.LEFT, expand=1, fill=tkinter.BOTH, padx=2)
+
+
+class PlayScreenInterface(object):
+
+    def __init__(self, master):
+        self.master = master
+        self.init_widgets()
+    
+    def init_widgets(self):
+        frame = tkinter.Frame(self.master, background="green")
+        frame.pack(side=tkinter.TOP, fill=tkinter.BOTH)
+
+
+tk = tkinter.Tk()
+tk.title("video player")
+play_button_interface = PlayBottonInterface(tk)
+play_screen_interface = PlayScreenInterface(tk)
+tk.mainloop()
