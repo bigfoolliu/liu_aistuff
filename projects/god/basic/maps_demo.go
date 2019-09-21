462321b9433a92bb88a00f2758793ffa78bde3c5
tonyliu <liurui@jiangxing.ai>
2019-09-21 13:25:25 +0800

    add

--- projects/god/basic/maps_demo.go
+++ projects/god/basic/maps_demo.go
@@ -3,25 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	// 创建一个map对象，类型字典
+	// 使用make创建一个map
 	m := make(map[string]int)
 
+	// 增加，修改键值对， k不存在为增加，k存在为修改
 	m["k1"] = 7
 	m["k2"] = 13
-
 	fmt.Println("map:", m)
-
-	v1 := m["k1"]
-	fmt.Println("v1: ", v1)
-
 	fmt.Println("len:", len(m))
 
+	// 删除键值对
 	delete(m, "k2")
 	fmt.Println("map:", m)
 
+	// 查找键值对
+	v1 := m["k1"]
+	v2 := m["k3"]
+	fmt.Println("v1:", v1, "v2:", v2) // 不存在默认给了0
+
+	// 这种不存在键值报false
 	_, prs := m["k2"]
 	fmt.Println("prs:", prs)
 
-	n := map[string]int{"foo": 1, "bar": 2}
+	// 直接使用map来创建
+	n := map[string]int{
+		"foo": 1,
+		"bar": 2,
+	}
 	fmt.Println("map:", n)
+
+	// map的遍历
+	for k, v := range n {
+		fmt.Printf("k:[%v] v:[%v]\n", k, v)
+	}
+
 }
