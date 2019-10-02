f6626f2228ea4a4d14ca9ab027dd8be4f7b9a777
bigfoolliu <liu15671677014@icloud.com>
2019-10-02 19:10:29 +0800

    最大子序列和增加动态规划解法

--- algorithms/leetcode/simple/53_maxinum_sum_subarray.py
+++ algorithms/leetcode/simple/53_maxinum_sum_subarray.py
@@ -56,9 +56,41 @@ def maxinum_sum_subarray2(array):
     return max_sum
 
 
+def maxinum_sum_subarray3(array):
+    """
+    使用动态规划进行求解
+
+    1.建立状态转移方程的模型
+        状态转移方程：
+        dp[i] = max(dp[i-1] + nums[i], nums[i])
+        dp[i]：当前位置i的最大子序列和
+        nums[i]: 数组的第i个元素
+
+        初始化:
+        dp[0] = nums[0]
+    
+    2. 将模型与当前的问题进行对应
+        curr_max_sum = max(curr_max_sum + nums[i], numx[i])
+        max_sum = max(curr_max_sum, max_sum)
+
+        初始化：
+        curr_max_sum = nums[0]
+
+    :param array: list, 整数数组
+    :return int
+    """
+    curr_max_sum = max_sum = array[0]
+    for i in range(1, len(array)):
+        curr_max_sum = max(curr_max_sum + array[i], array[i])
+        max_sum = max(curr_max_sum, max_sum)
+    return max_sum
+
+
 if __name__ == "__main__":
-    # array = [-2,1,-3,4,-1,2,1,-5,4]
-    array = [2, -3, 4, -5, 1]
+    array = [-2,1,-3,4,-1,2,1,-5,4]
+    # array = [2, -3, 4, -5, 1]
     print(maxinum_sum_subarray1(array))
 
     print(maxinum_sum_subarray2(array))
+
+    print(maxinum_sum_subarray3(array))
