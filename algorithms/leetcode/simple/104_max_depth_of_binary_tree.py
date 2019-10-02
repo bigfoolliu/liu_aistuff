d47eec928391a7e17562407b75d77f24c0459e16
bigfoolliu <liu15671677014@icloud.com>
2019-10-03 01:45:06 +0800

    增加二叉树md以及求最大深度leetcode初始化

---
+++ algorithms/leetcode/simple/104_max_depth_of_binary_tree.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+求取二叉树的最大深度。
+"""
+
+
+def max_depth_of_binary_tree(array):
+    """
+    :param array: list
+    return int
+    """
+    pass
+
+
+if __name__ == "__main__":
+    array = [3, 9, 20, None, None, 15, 7]
+    print(max_depth_of_binary_tree(array))
