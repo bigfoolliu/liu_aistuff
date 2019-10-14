ba46b4be25f74262d158c297ad1d76afea140338
bigfoolliu <liu15671677014@icloud.com>
2019-10-15 03:08:26 +0800

    增加大文件操作

---
+++ python/python/files/read_big_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+流式读取大文件
+with open方式读取大文件，若所有字符串在同一行则将消耗大量的内存
+"""
+
+
+def chunked_file_reader(fp, block_size=1024*8):
+    """
+    生成器函数，分块读取文件
+    fp: 待操作的文件对象
+    block_size: 每次读取的块的大小
+    """
+    while True:
+        chunk = fp.read(block_size)
+        if not chunk:
+            break
+        yield chunk
