dd49ad828470d15abb54d97711632d0977eab255
bigfoolliu <liu15671677014@icloud.com>
2019-10-17 17:11:11 +0800

    leetcode增加28寻找needle

---
+++ algorithms/leetcode/simple/28_implement_strStr.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+思路：python列表的使用
+"""
+
+
+def strStr(haystack, needle):
+    """
+    :param hsystack: str
+    :param needle: str
+    :return int
+    """
+    for i in range(0, len(haystack) - len(needle) + 1):
+        if haystack[i:i+len(needle)] == needle:
+            return i
+    return -1
+
+
+print(strStr("hello", "ll"))
+print(strStr("aaaaa", "bba"))
