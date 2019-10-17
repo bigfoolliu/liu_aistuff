53e1f055cddc3b6a523d4640d3def36ea7bf7634
bigfoolliu <liu15671677014@icloud.com>
2019-10-17 14:34:18 +0800

    leetcode增加13罗马数字转整数

---
+++ algorithms/leetcode/simple/13_roman_to_integer.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+Example 3:
+
+Input: "IX"
+Output: 9
+Example 4:
+
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 5:
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+思路：找到罗马数字的计算规律
+"""
+
+
+def roman_to_integer(s):
+    """
+    :param s: str
+    return int
+    """
+    rom_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    num = 0  # 结果
+    prev = 0  # 前一个数字
+    for i in s:
+        if prev and 2 * prev < rom_dict[i]:
+            num += (rom_dict[i] - 2 * prev)
+        else:
+            num += rom_dict[i]
+        prev = rom_dict[i]
+    return num
+
+
+print(roman_to_integer("III"))
+print(roman_to_integer("LVIII"))
+print(roman_to_integer("MCMXCIV"))
