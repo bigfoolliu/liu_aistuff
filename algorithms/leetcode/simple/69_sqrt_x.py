cec0dbf7a4bae67e4cfea2a6e9485aa62bd142da
bigfoolliu <liu15671677014@icloud.com>
2019-10-17 22:22:46 +0800

    leetcode增加69求平方根

---
+++ algorithms/leetcode/simple/69_sqrt_x.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+
+计算一个非负整数的平方根，没有整数平方根的时候返回整数部分
+
+思路：
+"""
+import math
+
+
+def my_sqrt(x):
+    """
+    :param x: int
+    :return: int
+    """
+    ret = int(str(math.sqrt(x)).split(".")[0])
+    return ret
+
+
+print(my_sqrt(4))
+print(my_sqrt(8))
