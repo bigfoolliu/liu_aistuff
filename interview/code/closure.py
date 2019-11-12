ff92a55cb0bd810dae80ca5f4eb7698b1de5336b
bigfoolliu <liu15671677014@icloud.com>
2019-11-12 19:36:42 +0800

    closure闭包修改

--- interview/code/closure.py
+++ interview/code/closure.py
@@ -6,14 +6,15 @@
 闭包的最基本实现
 """
 
+
 def my_add(num1):
     def real_add(num2):
+        # 内函数引用了外函数的临时变量
         return num1 + num2
     return real_add
 
 
-p = my_add(1)
+p = my_add(1)  # p结果为real_add
 
-print(p(2))
+print(p(2))  # 2传给了num2
 assert p(2) == 3
-
