8f10d1dcb5c1692bc8fd9b0e4b26e5dd2365a3ff
bigfoolliu <liu15671677014@icloud.com>
2019-12-04 23:40:54 +0800

    pillow模块示例修改

--- python/modules/pillow/pillow_module.py
+++ python/modules/pillow/pillow_module.py
@@ -9,13 +9,9 @@ pillow库
 空间转换。
 
 """
-import csv  # csv模块可以加载.csv文件
 import os  # os库提供了使用各种操作系统功能的接口
-import sys  # sys库提供了许多函数和变量来处理 Python 运行时环境的不同部分。
 
-import numpy
-from matplotlib import pyplot
-from PIL import Image  # 从库中导入该类
+from PIL import Image, ImageDraw, ImageFont
 
 
 """
@@ -26,29 +22,45 @@ Image类
 
 
 def display_demo():
-    """展示图像示例"""
-    # 从文件加载图像，加载成功将返回一个Image对象，不需要知道文件格式
-    im = Image.open('../res/image_test.jpg')  # 从文件中加载图像
-
-    # 通过示例属性查看文件的内容
-    print(im.format, im.size, im.mode)
-
-    # 通过show()函数就可以显示图像，但效率不高
+    """
+    展示图像示例
+    从文件加载图像，加载成功将返回一个Image对象，不需要知道文件格式
+    """
+    im = Image.open("image_test.jpg")  # 从文件中加载图像
+    print(im.format, im.size, im.mode)  # 查看文件的内容,格式，尺寸，模式
     im.show()
 
 
-"""
-读写图像
-使用save()方法保存文件，保存文件的时候文件名变得重要了。除非你指定格式，
-否则这个库将会以文件名的扩展名作为格式保存。
-"""
-
-
 def save_demo():
-    """保存图像示例"""
+    """
+    使用save()方法保存文件，保存文件的时候文件名变得重要了。除非你指定格式，
+    否则这个库将会以文件名的扩展名作为格式保存。
+    """
     # 将文件转换为.png格式
-    im.save('../res/image_test.png')
+    im = Image.open("./image_test.jpg")
+    if not os.path.exists("./image_test.png"):
+        im.save("image_test.png")
+
+
+def create_demo():
+    """创建一张图片"""
+    if not os.path.exists("./create_demo.jpg"):
+        im = Image.new("RGB", (200, 200), "white")  # 指定模式，大小和颜色
+        im.save("create_demo.jpg")
+
+
+def font_demo():
+    """使用特定字体"""
+    im = Image.open("./create_demo.jpg")
+    font = ImageFont.truetype("./SimHei.ttf", size=20)  # 加载指定字体
+    draw = ImageDraw.Draw(im)  # 将图片转换为可编辑
+    draw.text((50, 30), "Test Text", font=font, fill="red")  # 输入文字
+    im.save("./font_demo.jpg")
+    im.show()
 
 
 if __name__ == "__main__":
-    display_demo()
+    # display_demo()
+    # save_demo()
+    # create_demo()
+    font_demo()
