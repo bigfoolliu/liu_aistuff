42dc8967940b99fe1e4085966b2eeb666aae1858
bigfoolliu <liu15671677014@icloud.com>
2019-12-15 17:12:31 +0800

    uuid模块修改

--- python/modules/uuid/uuid_module.py
+++ python/modules/uuid/uuid_module.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 # author: bigfoolliu
 
+
 """
 通过uuid库生成uuid
 
@@ -11,20 +12,36 @@ uuid:
 固定的大小：128位
 """
 
+
 import uuid
 
-# 基于时间戳以及mac地址,在Global分布式环境下使用较好
-id1 = uuid.uuid1()
-print(id1)
 
-# 基于名字的SHA-1散列值,名字有唯一性要求使用较好
-id5 = uuid.uuid5(uuid.NAMESPACE_DNS, "hello")
-print(id5)
+def uuid1_demo():
+    """基于时间戳以及mac地址,在Global分布式环境下使用较好"""
+    id1 = uuid.uuid1()
+    print(id1)
 
-# 基于名字的MD5散列值,名字有唯一性要求使用较好
-id3 = uuid.uuid3(uuid.NAMESPACE_DNS, 'hello')
-print(id3)
 
-# 基于随机数,概率性重复，最好不要使用
-id4 = uuid.uuid4()
-print(id4)
+def uuid3_demo():
+    """基于名字的MD5散列值,名字有唯一性要求使用较好"""
+    id3 = uuid.uuid3(uuid.NAMESPACE_DNS, 'hello')
+    print(id3)
+
+
+def uuid4_demo():
+    """基于随机数,概率性重复，最好不要使用"""
+    id4 = uuid.uuid4()
+    print(id4)
+
+
+def uuid5_demo():
+    """基于名字的SHA-1散列值,名字有唯一性要求使用较好"""
+    id5 = uuid.uuid5(uuid.NAMESPACE_DNS, "hello")
+    print(id5)
+
+
+if __name__ == "__main__":
+    uuid1_demo()
+    uuid3_demo()
+    uuid4_demo()
+    uuid5_demo()
