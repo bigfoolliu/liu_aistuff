fc699bbc3524ce7aca9912e499e01afbbe36c347
bigfoolliu <liu15671677014@icloud.com>
2019-12-29 00:10:39 +0800

    leetcode增加189

---
+++ algorithms/leetcode/easy/189_rotate_array.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+Note:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+说明:
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+要求使用空间复杂度为 O(1) 的 原地 算法。
+"""
+
+
+def rotate_array(nums, k):
+    """
+    :param nums: list
+    :return: list
+    """
+    for i in range(k):
+        nums.insert(0, nums.pop())
+    return nums
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    print(rotate_array(nums, k))
