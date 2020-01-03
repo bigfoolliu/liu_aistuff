f027fcff3eaa687f531b3bc2a2a2fc1f001734e2
bigfoolliu <liu15671677014@icloud.com>
2020-01-03 17:29:26 +0800

    python enum增加修改示例

--- python/python/inner_functions/enum_demo.py
+++ python/python/inner_functions/enum_demo.py
@@ -6,7 +6,7 @@
 """
 python枚举类和普通类的不同
 
-http://c.biancheng.net/view/2305.html
+- python Enum枚举类的使用: https://www.cnblogs.com/skaarl/p/10279428.html
 
 1.枚举类值不可变
 2.具有防止相同标签的功能，不同标签的值可以相同
@@ -18,21 +18,26 @@ http://c.biancheng.net/view/2305.html
 
 from enum import Enum
 
-# 定义枚举类方式一：直接使用
-# 创建一个Month类型的枚举类
-months = Enum("Month", ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
-# 访问枚举的成员
-print(months)
-print(months.Jan)  # 引用一个常量
-print(months.Jan.value)
-print(months.Jan.name)
-print(months(1))
-print(months["Jan"])
+def basic_demo():
+    # 定义枚举类方式一：直接使用
+    # 创建一个Month类型的枚举类
+    months = Enum("Month", ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
-# 枚举所有的成员
-for name, member in months.__members__.items():
-    print("name: ", member.name, "value:", member.value)  # value属性是自动赋值给成员的int常量，默认从1计数
+    # 访问枚举的成员
+    print(months)
+    print(months.Jan)  # 引用一个常量
+
+    # 获取该枚举对象的值和变量名
+    print(months.Jan.value)
+    print(months.Jan.name)
+
+    print(months(1))
+    print(months["Jan"])
+
+    # 枚举所有的成员
+    for name, member in months.__members__.items():
+        print("name: ", member.name, "value:", member.value)  # value属性是自动赋值给成员的int常量，默认从1计数
 
 
 # 定义枚举类方式二：继承使用
@@ -40,3 +45,15 @@ class Gender(Enum):
     """自定义一个枚举类"""
     Male = 0
     Female = 1
+
+
+def inhert_demo():
+    male = Gender(0)
+    female = Gender(1)
+    print(male, type(male))
+    print(female, type(female))
+
+
+if __name__ == "__main__":
+    # basic_demo()
+    inhert_demo()
