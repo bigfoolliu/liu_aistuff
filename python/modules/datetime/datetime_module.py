4f4df06c9a558e687a0acb63dc81b243aeef4b73
刘瑞 <liurui07@inspur.com>
2020-02-13 18:05:15 +0800

    datetime增加内容

--- python/modules/datetime/datetime_module.py
+++ python/modules/datetime/datetime_module.py
@@ -31,14 +31,26 @@ def str_to_timestamp_demo():
 
 
 def timestamp_to_datetime_demo():
-    # 时间戳转换为datetime时间
+    """时间戳转换为datetime时间"""
     time_stamp = int("1581696000")
     datetime_time = datetime.fromtimestamp(time_stamp)
     print("datetime_time:{} type:{}".format(datetime_time, type(datetime_time)))
 
 
+def get_add_reduce_time_demo(time_str, time_format, time_delta):
+    """将字符串时间增加或者减少之后再转换为字符输出"""
+    unix_time = time.mktime(time.strptime(time_str, time_format))
+    datetime_obj = datetime.fromtimestamp(unix_time)
+
+    ret_time = datetime_obj + timedelta(seconds=time_delta)
+    ret_time_str = ret_time.strftime(time_format)
+    return ret_time_str
+
+
 if __name__ == "__main__":
     # get_utc_demo()
-    str_to_timestamp_demo()
+    # str_to_timestamp_demo()
     # timestamp_to_datetime_demo()
+    print(get_add_reduce_time_demo('2020-02-15 00:00', "%Y-%m-%d %H:%M", 15*60))
+    print(get_add_reduce_time_demo('2020-02-15 00:00', "%Y-%m-%d %H:%M", -15*60))
 
