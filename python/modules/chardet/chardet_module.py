0ea27970ac3d9d02898c6e8d00aba7717e8dcf46
刘瑞 <liurui07@inspur.com>
2020-02-28 00:14:40 +0800

    python模块增加chardet检测字符编码示例

---
+++ python/modules/chardet/chardet_module.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+chardet模块使用
+
+自动识别编码
+"""
+
+
+import chardet
+
+
+def detect_demo():
+    """基本识别示例"""
+    code = b"\xc8\xcb\xc9\xfa\xbf\xe0\xb6\xcc\xa3\xac\xce\xd2\xd3\xc3Python"
+    ret = chardet.detect(code)  # 查看编码信息
+    print(ret)
+
+    # 使用解析出来的编码来decode
+    decoded_ret = code.decode(ret["encoding"])
+    print(decoded_ret)
+
+
+if __name__ == "__main__":
+    detect_demo()
+
