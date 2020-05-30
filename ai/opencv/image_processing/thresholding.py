6cd10acf2f363129e6ea60d038805f80a81409e4
bigfoolliu <liu15671677014@icloud.com>
2020-05-31 00:01:11 +0800

    format

--- ai/opencv/image_processing/thresholding.py
+++ ai/opencv/image_processing/thresholding.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#author: bigfoolliu
+# author: bigfoolliu
 
 
 """
@@ -14,24 +14,23 @@ cv2.THRESH_TOZERO
 cv2.THRESH_TOZERO_INV
 """
 
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread("../images/gradient.jpg", 0)
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 
 for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
+    plt.xticks([]), plt.yticks([])
 
 plt.show()
