3dd84109b3490deb0ae3f0a4323c6a0c9f30715a
bigfoolliu <liu15671677014@icloud.com>
2020-06-28 00:44:25 +0800

    python add

---
+++ python/python/inner_functions/vars_demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+vars内置函数
+
+返回对象的属性和属性值的字典对象
+"""
+
+
+class Demo(object):
+    name = "demo class"
+
+
+def basic_demo():
+    print(vars())
+    print(type(vars()))
+
+    print(vars(Demo))
+    print(vars(Demo()))
+
+
+if __name__ == "__main__":
+    basic_demo()
+
