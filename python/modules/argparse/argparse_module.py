0cbd951cb5f04af9c46908b45a5ca81486385ae8
bigfoolliu <liu15671677014@icloud.com>
2020-07-11 23:32:45 +0800

    argparse增加内容

--- python/modules/argparse/argparse_module.py
+++ python/modules/argparse/argparse_module.py
@@ -5,6 +5,9 @@
 
 """
 argparse模块的简单使用
+
+- 解析命令行的位置参数或者可选参数
+- 详细用法官方文档：https://docs.python.org/zh-cn/3/library/argparse.html#argparse.ArgumentParser
 """
 
 
@@ -12,15 +15,39 @@ import argparse
 
 
 def basic_demo():
-    parser = argparse.ArgumentParser(description="receive arguments")
-    # 构建一个组来管理参数
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--mqtt-subscribe")
-    group.add_argument("--mqtt-publish")
+    # 1.创建一个解析器对象, 运行 `python3 argparse_module.py -h` 返回的信息则是description的内容
+    parser = argparse.ArgumentParser(description='接受一系列参数')
 
+    # 2.添加位置参数,默认是必填的，否则会报错, 用法是不用带 -， 使用时候 `python3 argparse_module.py ha`
+    parser.add_argument('e')
+
+    # 3.可选参数，两种方式，长参数 --version 或者短参数-V， 两种方式可以同存，也可以只存在一个
+    # help参数帮助，action='store_true'时候，--version 后面不需要跟具体参数
+    parser.add_argument('-V', '--version', help='输出版本', action='store_true')
+
+    # 4.指定输入参数的类型，默认为str, 且输入类型错误会报错，choices 限定可选的值有哪些, default设置默认值
+    parser.add_argument('-x', help='输入整数', type=int, choices=[0, 1, 2], default=2)
+
+    # 5.增加互斥参数，即参数不能同时出现
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a', action='store_true')
+    group.add_argument('-b', action='store_true')
+
+    # 解析参数
     args = parser.parse_args()
+
     print(args, type(args))
+    print(f'e: {args.e}')
+    if args.version:
+        print('version: 0.0.1')
+    print(f'x + x: {args.x + args.x}')
+
+    if args.a or args.b:
+        group_arg = args.a if args.a else args.b
+        print(f'group arg: {group_arg}')
+    else:
+        print('group_arg: None')
 
 
 if __name__ == "__main__":
-    pass
+    basic_demo()
