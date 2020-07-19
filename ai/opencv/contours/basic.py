6af0c1e87f15f9403d07aafe9d12f6010155b40c
bigfoolliu <liu15671677014@icloud.com>
2020-07-19 19:08:34 +0800

    format

--- ai/opencv/contours/basic.py
+++ ai/opencv/contours/basic.py
@@ -17,13 +17,13 @@ ret, thresh = cv2.threshold(gray_img, 127, 255, 0)
 
 # thresh是轮廓源图，cv2.RETR_TREE是轮廓检测模型，cv2.CHAIN_APPROX_SIMPLE是轮廓近似函数
 # 输出图像，轮廓和等级
-contour_img, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # 轮廓展示效果图
 contour_effect_img1 = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 contour_effect_img2 = cv2.drawContours(img, contours, 3, (0, 255, 0), 3)
 
-images = [img, gray_img, contour_img, contour_effect_img1, contour_effect_img2]
+images = [img, gray_img, contour_effect_img1, contour_effect_img2]
 titles = ["original img", "gray img", "contour img", "contour effect img1", "contour effect img2"]
 
 for i in range(len(images)):
