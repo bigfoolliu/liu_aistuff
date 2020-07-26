f172f03e9b778258569699dc6d8f763bd2c1bfec
bigfoolliu <liu15671677014@icloud.com>
2020-07-26 18:32:03 +0800

    pythn module pyecharts format

--- python/modules/pyecharts/pyecharts_module.py
+++ python/modules/pyecharts/pyecharts_module.py
@@ -5,16 +5,30 @@
 
 """
 pyecharts模块实现数据可视化
+
+- github地址： https://github.com/pyecharts/pyecharts/
 """
 
-from pyecharts import Bar
+from pyecharts.charts import Bar
+from pyecharts import options as opts
 
 
-def basic_demo():
-    bar = Bar("pyecharts demo主标题", "副标题")
-    bar.add("服装", ["衬衫", "羊毛衫", "裤子"])
+def bar_demo():
+    # V1 版本开始支持链式调用
+    # bar = (Bar().add_xaxis(["衬衫", "毛衣", "领带", "裤子", "风衣", "高跟鞋", "袜子"]).
+    # add_yaxis("商家A", [114, 55, 27, 101, 125, 27, 105]).
+    # add_yaxis("商家B", [57, 134, 137, 129, 145, 60, 49]).
+    # set_global_opts(title_opts=opts.TitleOpts(title="某商场销售情况")))
+    # bar.render()
+
+    # 不习惯链式调用的开发者依旧可以单独调用方法
+    bar = Bar(t )
+    bar.add_xaxis(["衬衫", "毛衣", "领带", "裤子", "风衣", "高跟鞋", "袜子"])
+    bar.add_yaxis("商家A", [114, 55, 27, 101, 125, 27, 105])
+    bar.add_yaxis("商家B", [57, 134, 137, 129, 145, 60, 49])
+    bar.set_global_opts(title_opts=opts.TitleOpts(title="某商场销售情况"))
     bar.render()
 
 
 if __name__ == "__main__":
-    basic_demo()
+    bar_demo()
