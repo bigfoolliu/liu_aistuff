4ec93f500700de88490f6fbbb1b508e2d74f2137
bigfoolliu <liu15671677014@icloud.com>
2020-10-16 10:33:48 +0800

    functools增加读取超大文件

--- language/python/modules/functools/functools_module.py
+++ language/python/modules/functools/functools_module.py
@@ -10,7 +10,7 @@ functools模块使用
 # 1.使用自带的缓存机制
 # maxsize：最多可以缓存多少个此函数的调用结果，如果为None，则无限制，设置为 2 的幂时，性能最佳
 # typed：若为 True，则不同参数类型的调用将分别缓存。
-from functools import lru_cache
+from functools import lru_cache, partial
 
 
 @lru_cache(maxsize=1024, typed=True)
@@ -25,5 +25,25 @@ def calculate_demo():
     calculate(1)  # 第二次调用的时候没有真正执行函数体，使用的缓存结果
 
 
+# 2.借助偏函数和iter函数流式读取超大文件
+# partial用法: http://www.xxf-home.net/archives/1004
+def read_from_file(file_name, block_size=1024 * 8):
+    """
+    :param file_name: 文件名称
+    :param block_size: 默认为8kb
+    :return: generator object
+    """
+    with open(file_name, 'r') as fp:
+        for chunk in iter(partial(fp.read, block_size), ''):
+            yield chunk
+
+
+def read_demo():
+    file_generator = read_from_file('../../python_introduction.md')
+    for i in file_generator:
+        print(i)
+
+
 if __name__ == "__main__":
-    calculate_demo()
+    # calculate_demo()
+    read_demo()
