915ab05396ada752afbe1ca411c53081e458ab37
bigfoolliu <liu15671677014@icloud.com>
2020-10-21 10:40:47 +0800

    增加类型注解

--- language/python/python/doc/doc_type_demo.py
+++ language/python/python/doc/doc_type_demo.py
@@ -16,6 +16,10 @@ python类型注解
 
 - 让别的程序员看得更明白
 - 让 IDE 了解类型，从而提供更准确的代码提示、补全和语法检查（包括类型检查，可以看到 str 和 float 类型的参数被高亮提示）
+
+使用特定的库对文件进行类型检查
+pip3 install mypy
+mypy test.py
 """
 
 
@@ -28,9 +32,20 @@ def add2(x, y):
     return x + y
 
 
+def add3():
+    # 变量的类型注解释
+    a: int = 1
+    l: list = [1, 'a']
+
+    from typing import List
+    li: List[int] = [1, 3]  # 指明一个全部由整数构成的列表
+    print(a, l, li)
+
+
 if __name__ == '__main__':
     print(add(2, 4))
     print(add('hello', 'world'))  # ide提示类型问题
     print(add2(2, 4))  # 不会有任何提示
 
     print(add.__annotations__)  # 可以查看注解
+    add3()
