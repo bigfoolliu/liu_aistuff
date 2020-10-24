18afb2d53ca64a3926f6caeb1998e03038aeb3a3
bigfoolliu <liu15671677014@icloud.com>
2020-10-24 21:12:03 +0800

    importlib add

--- language/python/modules/importlib/importlib_module.py
+++ language/python/modules/importlib/importlib_module.py
@@ -7,18 +7,13 @@
 importlib使用
 
 - 博客园python importlib使用: https://www.cnblogs.com/bad-robot/p/9746870.html
+- 以及导包的多种方式
 """
 
 import importlib
-
-
-def basic_demo():
-    _service = "importlib_test_module.TestClass".split(".")
-    print(_service)
-    _lib = importlib.import_module(".".join(_service[0:-1]))
-    print(_lib, type(_lib))
-    print(getattr(_lib, _service[-1]))
+import sys
+import os
 
 
 if __name__ == "__main__":
-    basic_demo()
+    pass
