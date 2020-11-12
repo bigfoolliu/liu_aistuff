a6362f84e022394bdceaf2679f116b24c770256f
bigfoolliu <liu15671677014@icloud.com>
2020-11-12 23:20:06 +0800

    gevent add

--- language/python/modules/gevent/gevent_module.py
+++ language/python/modules/gevent/gevent_module.py
@@ -10,10 +10,10 @@ gevent模块示例
 - Python通过yield提供了对协程的基本支持，但是不完全。而第三方的gevent为Python提供了比较完善的协程支持。
 
 
-基本思想是：
+gevent基本思想是：
 
-当一个greenlet遇到IO操作时，比如访问网络，就自动切换到其他的greenlet，
-等到IO操作完成，再在适当的时候切换回来继续执行。
+1. gevent是python的一个并发框架，以微线程greenlet为核心，使用了epoll事件监听机制
+2. 当一个greenlet遇到IO操作时，比如访问网络，就自动切换到其他的greenlet，等到IO操作完成，再在适当的时候切换回来继续执行。
 由于IO操作非常耗时，经常使程序处于等待状态，有了gevent为我们自动切换协程，就保证总有greenlet在运行，而不是等待IO。
 
 由于切换是在IO操作时自动完成，所以gevent需要修改Python自带的一些标准库，这一过程在启动时通过monkey patch完成。
@@ -32,6 +32,9 @@ gevent常用方法：
 - monkey.patch_all()	非常重要，会自动将python的一些标准模块替换成gevent框架
 """
 
+
+import datetime
+import requests
 from gevent import monkey
 import gevent
 
@@ -59,5 +62,23 @@ def basic_demo():
     gevent.joinall([t1, t2])
 
 
+def task_get_url(url):
+    print(f'time: {datetime.datetime.now()} url: {url}')
+    resp = requests.get(url)
+    print(f'time: {datetime.datetime.now()} url: {url}')
+    print(f'{len(resp.text)} bytes received, {url}')
+
+
+def demo2():
+    gevent.joinall([
+        gevent.spawn(task_get_url, 'https://www.python.org/'),
+        gevent.spawn(task_get_url, 'https://github.com/'),
+        gevent.spawn(task_get_url, 'https://www.baidu.com/'),
+    ])
+
+
+
 if __name__ == "__main__":
-    basic_demo()
+    # basic_demo()
+    demo2()
+
