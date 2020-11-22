b02b6eb9d8c7a2fef2fc52705e29905fb3661626
bigfoolliu <liu15671677014@icloud.com>
2020-11-22 23:40:49 +0800

    leetcode add

---
+++ algorithms/leetcode/contest/83/832.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: bigfoolliu
+
+
+"""
+"""
+
+
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+
+    def test1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
