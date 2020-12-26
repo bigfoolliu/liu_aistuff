81a0d0114e554019de31701b0df4216e10373369
bigfoolliu <liu15671677014@icloud.com>
2020-12-26 18:19:56 +0800

    shell add more

--- language/shell/condition.sh
+++ language/shell/condition.sh
@@ -11,3 +11,29 @@ elif [ "$name" == "tony" ]
 then
     echo "hello tony"
 fi
+
+
+# 与或非，注意这里的true和false和0与1是刚好相反的
+# 与
+if true && true
+then
+    echo "true"
+fi
+
+# 或
+if true || false
+then
+    echo "true"
+fi
+
+
+# 非
+if ! false
+then
+    echo "false"
+fi
+
+
+# 如果ping通则执行后面的命令
+ping -c 1 www.baidu.com && echo "ping baidu sccced"
+
