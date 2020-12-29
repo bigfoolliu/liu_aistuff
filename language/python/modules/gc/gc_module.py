002691b9e8c1fe089349905c3376090c6d2b5e5d
bigfoolliu <liu15671677014@icloud.com>
2020-12-29 10:01:21 +0800

    gc add more

--- language/python/modules/gc/gc_module.py
+++ language/python/modules/gc/gc_module.py
@@ -4,6 +4,20 @@
 
 
 """
+
+Python中的垃圾回收是以引用计数为主，分代收集为辅。
+
+1、导致引用计数+1的情况
+    对象被创建，例如a=23
+    对象被引用，例如b=a
+    对象被作为参数，传入到一个函数中，例如func(a)
+    对象作为一个元素，存储在容器中，例如list1=[a,a]
+2、导致引用计数-1的情况
+    对象的别名被显式销毁，例如del a
+    对象的别名被赋予新的对象，例如a=24
+    一个对象离开它的作用域，例如f函数执行完毕时，func函数中的局部变量（全局变量不会）
+    对象所在的容器被销毁，或从容器中删除对象
+
 gc模块示例
 
 - python gc模块介绍: https://blog.csdn.net/qq_38260497/article/details/87879064
@@ -23,10 +37,40 @@ gc.get_threadshold()
 """
 
 import gc
+import sys
+
+
+def get_ref_times():
+    """
+    查看对象被应用的次数
+    """
+    a = 'what the hell'
+    print(sys.getrefcount(a))
+    del a
+    # print(sys.getrefcount(a))
+
+
+class A(object):
+
+    pass
 
 
 def basic_demo():
-    pass
+    a1 = A()
+    a2 = A()
+
+    a1.a = a2  # 循环引用，会导致内存泄漏
+    a2.a = a1
+
+    del a1
+    del a2
+
+    print(gc.garbage)  # 显示垃圾
+    print('*' * 100)
+    gc.collect()  # 显式的执行垃圾回收
+    print('*' * 100)
+    print(gc.garbage)
+
 
 
 def collect_demo():
@@ -38,4 +82,9 @@ def collect_demo():
 
 if __name__ == "__main__":
     # TODO:
-    pass
+    # get_ref_times()
+
+
+    gc.set_debug(gc.DEBUG_LEAK)  # 设置gc的模块的日志,进行内存泄漏的检查
+    basic_demo()
+
